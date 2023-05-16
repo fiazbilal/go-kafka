83a239a7586f6604b84abaf39b0df5bf4fbc0931
fiazbilal <fiaz.bilal@mslm.io>
2023-05-16 14:18:23 +0500

    making ready company create ep

--- api/ep_company_create.go
+++ api/ep_company_create.go
@@ -2,6 +2,6 @@ package api
 
 import "net/http"
 
-func createCompany(w http.ResponseWriter, r *http.Request) {
+func CompanyCreate(w http.ResponseWriter, r *http.Request) {
 	// Implement the logic for creating a company
 }
--- api/main.go
+++ api/main.go
@@ -15,13 +15,30 @@ func Main() {
 	log.Fatal(http.ListenAndServe(listenUrl, srvr))
 }
 
-func MainHandler(w http.ResponseWriter, req *http.Request) {
+func MainHandler(resp http.ResponseWriter, req *http.Request) {
 
 	url := req.URL.Path
 	if url != "/" {
 		url = strings.TrimRight(req.URL.Path, "/")
 	}
 
-	fmt.Printf("URL: %s", url)
+	fmt.Printf("%v %v", req.Method, url)
 
+	// Routing & authorization.
+	// revive:disable
+	if req.Method == "GET" {
+	} else if req.Method == "POST" {
+		if url == "/api/mjob/v1/job/create" {
+			CompanyCreate(req, resp)
+			return
+		} else
+			resp.Send(http.StatusNotFound)
+			return
+		}
+	} else if req.Method == "PATCH" {
+	} else if req.Method == "DELETE" {
+	} else {
+		resp.Send(http.StatusMethodNotAllowed)
+		return
+	}
 }
