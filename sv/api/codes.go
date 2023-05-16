997b7a129c5413606ca07aee296a52860fd6bcd9
fiazbilal <fiaz.bilal@mslm.io>
2023-05-16 14:56:12 +0500

    standard codes for the project

---
+++ sv/api/codes.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"company/sv"
+)
+
+const (
+	// Reserved
+	RC_E_NO_BODY   sv.RespCode = 2999
+	RC_E_MALFORMED sv.RespCode = 2998
+	RC_E_RATELIMIT sv.RespCode = 2997
+
+	// POST /api/v1/company/create
+	RC_COMPANY_CREATE sv.RespCode = 1000
+)
